665d32ca6638a726f74d05103a2c427924ec2242
J N <jacob.matthew.nash@gmail.com>
2021-12-03 20:04:20 +0000

    day 2 part 1

---
+++ day2/DiveController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace day2
+{
+    class DiveController
+    {
+
+
+        static void Main(string[] args)
+        {
+            Submarine submarine = new Submarine();
+            List<(string, int)> commands = getCommands(@"../../../input.txt");
+            submarine.ExicuteCommands(commands);
+            int answer = submarine.horizontalPos * submarine.verticalPos;
+            Console.WriteLine(answer);
+
+            
+        }
+
+        static List<(string,int)> getCommands(string file)
+        {
+            List<(string, int)> commands = new List<(string, int)>();
+            foreach (string line in System.IO.File.ReadLines(file))
+            {
+                var substing = line.Split(" ");
+                commands.Add((substing[0], int.Parse(substing[1])));
+            }
+            return commands;
+        }
+    }
+}
---
+++ day2/Submarine.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day2
+{
+    class Submarine
+    {
+        internal int horizontalPos { get; private set; } = 0;
+        internal int verticalPos { get; private set; } = 0;
+
+
+        internal void ExicuteCommands(List<(string, int)> commands)
+        {
+            foreach((string command,int unit) command in commands)
+            {
+                switch (command.command)
+                {
+                    case "forward":
+                        horizontalPos += command.unit;
+                        break;
+                    case "up":
+                        verticalPos -= command.unit;
+                        break;
+                    case "down":
+                        verticalPos += command.unit;
+                        break;
+                    default:
+                        break;
+
+                }                    
+            }
+        }
+    }
+}
